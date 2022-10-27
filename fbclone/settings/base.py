103d6976b40f5fc2d59967f0093b0b6d5a615931
poonamk3 <poonamk@thoughtwin.com>
2022-10-27 13:54:15 +0530

    dfs

--- fbclone/settings/base.py
+++ fbclone/settings/base.py
@@ -139,14 +139,16 @@ WSGI_APPLICATION = 'fbclone.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-"""
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgres://vagrant:vagrant@localhost:5432/microblog')
 }
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
